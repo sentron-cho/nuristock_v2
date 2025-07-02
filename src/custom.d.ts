24f39bc617cf9d0734cdd84e1d6bb2b2ebcdcb03
sentron <sentron@daum.net>
2025-07-02 21:32:56 +0900

    초기설정

---
+++ src/custom.d.ts
@@ -0,0 +1,9 @@
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.gif';
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
