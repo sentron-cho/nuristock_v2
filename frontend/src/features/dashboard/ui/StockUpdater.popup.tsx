2858148d440003be988be62ecd72890e8876e61c
sentron <sentron@daum.net>
2025-07-16 18:51:58 +0900

    도커 설정 수정

--- frontend/src/features/dashboard/ui/StockUpdater.popup.tsx
+++ frontend/src/features/dashboard/ui/StockUpdater.popup.tsx
@@ -3,18 +3,36 @@ import { Dialog } from '@entites/Dialog';
 import { useForm } from 'react-hook-form';
 import { ST } from '@shared/config/kor.lang';
 import { DashboardItemType as DataType } from '@features/dashboard/api/dashboard.dto';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export const StockUpdaterPopup = ({ item, onClose }: { item?: DataType; onClose: (isOk: boolean) => void }) => {
-	const forms = useForm<{ textInput: string }>({ defaultValues: { textInput: item?.name } });
+	const forms = useForm<{ title: string }>({
+		defaultValues: { title: item?.name },
+		resolver: zodResolver(
+			z.object({
+				title: z.string({ message: ST.PLEASE_INPUT }),
+			})
+		),
+	});
 	console.log(item);
 
 	const onClickClose = (isOk: boolean) => {
-		if (isOk) {
-			if (!forms?.getValues('textInput')) {
-				forms.setError('textInput', { message: ST.PLEASE_INPUT });
-				return;
+		// if (isOk) {
+		// 	if (!forms?.getValues('textInput')) {
+		// 		forms.setError('textInput', { message: ST.PLEASE_INPUT });
+		// 		return;
+		// 	}
+		// }
+
+		forms?.handleSubmit(
+			(forms) => {
+				console.log('[success]', { forms });
+			},
+			(error) => {
+				console.log('[error]', { error });
 			}
-		}
+		)();
 
 		onClose?.(isOk);
 	};
@@ -25,7 +43,14 @@ export const StockUpdaterPopup = ({ item, onClose }: { item?: DataType; onClose:
 
 	return (
 		<Dialog title={ST.UPDATE} onClose={onClickClose}>
-			<TextFieldForm label={ST.STOCK_TITLE} size='small' id='textInput' formMethod={forms} onClearError={onClearError} maxLength={10} />
+			<TextFieldForm
+				label={ST.STOCK_TITLE}
+				size='small'
+				id='title'
+				formMethod={forms}
+				onClearError={onClearError}
+				maxLength={10}
+			/>
 		</Dialog>
 	);
 };
