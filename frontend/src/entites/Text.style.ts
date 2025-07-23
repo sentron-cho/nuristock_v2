751dff6f998708869de5a19697a5772fb0045e78
sentron <sentron@daum.net>
2025-07-23 22:27:50 +0900

    오류 수정

--- frontend/src/entites/Text.style.ts
+++ frontend/src/entites/Text.style.ts
@@ -1,6 +1,5 @@
 import { Typography } from '@mui/material';
 import { styled } from '@styles/stitches.config';
-import Flex from './Flex';
 
 export const StyledText = styled(Typography, {
 	'&.xxs': {
