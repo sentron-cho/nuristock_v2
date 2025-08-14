711e6bfa3c287a88be9d3f447c676f9948e0579a
sentron <sentron@daum.net>
2025-08-14 20:50:06 +0900

    저장

--- frontend/src/layouts/ui/Header.ui.tsx
+++ frontend/src/layouts/ui/Header.ui.tsx
@@ -1,100 +1,14 @@
-// import React, { useEffect, useMemo, useState } from 'react';
-// import { Link, useLocation, useNavigate } from 'react-router-dom';
-// import Flex from '@entites/Flex';
-// import clsx from 'clsx';
-// import Typography from '@mui/material/Typography';
-// import IconMenu from '@mui/icons-material/Menu';
-// import IconMenuOpen from '@mui/icons-material/MenuOpen';
-// import IconLogo from '@mui/icons-material/MonetizationOn';
-// import { StyledHeader } from '../style/Header.style';
-// import { ST } from '@shared/config/kor.lang';
-// import { SCREEN } from '@shared/config/default.config';
-// import { Menus } from '@layouts/data/menu.data';
-
-// // === Component ===
-// const Header: React.FC = () => {
-// 	const navigate = useNavigate();
-// 	const { pathname } = useLocation();
-// 	const [isOpen, setOpen] = useState(false);
-
-// 	// ✅ 윈도우 리사이즈 시 메뉴 닫기
-// 	useEffect(() => {
-// 		const handleResize = () => {
-// 			const width = window.innerWidth;
-// 			width > SCREEN.MOBILE && isOpen && setOpen(false);
-// 		};
-
-// 		window.addEventListener('resize', handleResize);
-// 		return () => window.removeEventListener('resize', handleResize);
-// 	}, [isOpen]);
-
-// 	const menu = useMemo(() => Menus(), []);
-
-// 	return (
-// 		<StyledHeader>
-// 			<Flex className='header-bar' justify={'center'} width={'100vw'}>
-// 				<nav className='nav'>
-// 					{menu?.map((a) => (
-// 						<Link key={a.value} to={a.value} className={clsx('link', { active: a.value === pathname })}>
-// 							{a.label}
-// 						</Link>
-// 					))}
-// 				</nav>
-// 				<Flex className='mobile' justify={'between'}>
-// 					<Flex className='title-bar' justify={'center'} flex={1} gap={4}>
-// 						<Flex
-// 							className='box'
-// 							width={'fit-content'}
-// 							onClick={() => {
-// 								setOpen(false);
-// 								navigate('/');
-// 							}}
-// 						>
-// 							<IconLogo fontSize='medium' />
-// 							<Typography className='title'>{ST.STOCK_DIARY}</Typography>
-// 						</Flex>
-// 					</Flex>
-// 					<button className='menu-button' onClick={() => setOpen((prev) => !prev)}>
-// 						{isOpen ? <IconMenuOpen fontSize={'large'} /> : <IconMenu fontSize={'large'} />}
-// 					</button>
-// 				</Flex>
-// 			</Flex>
-
-// 			{isOpen && (
-// 				<Flex className='mobile-menu' direction={'column'}>
-// 					<Flex gap={4} className='menu-li' direction={'column'} align={'start'}>
-// 						{menu?.map((a) => (
-// 							<Link
-// 								key={a.value}
-// 								to={a.value}
-// 								className={clsx('link', { active: a.value === pathname })}
-// 								onClick={() => setOpen(false)}
-// 							>
-// 								{a.label}
-// 							</Link>
-// 						))}
-// 					</Flex>
-// 					<div className='menu-bg' onClick={() => setOpen(false)} />
-// 				</Flex>
-// 			)}
-// 		</StyledHeader>
-// 	);
-// };
-
-// export default Header;
-
 import React, { useEffect, useMemo, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Flex from '@entites/Flex';
 import clsx from 'clsx';
 import Typography from '@mui/material/Typography';
 import IconLogo from '@mui/icons-material/MonetizationOn';
-// import { StyledHeader } from '../style/Header.style';
 import { ST } from '@shared/config/kor.lang';
 import { SCREEN } from '@shared/config/default.config';
 import { Menus } from '@layouts/data/menu.data';
 import { URL } from '@shared/config/url.enum';
-import { IconArrowLeft, IconArrowRight, IconForward } from '@entites/Icons';
+import { IconForward } from '@entites/Icons';
 import { styled } from '@styles/stitches.config';
 
 export const StyledHeader = styled('div', {
